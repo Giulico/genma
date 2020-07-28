865e15cf208f507b61bec7675c79a75888d59d7f
giuliocollesei <giuliocollesei@gmail.com>
2020-07-28 22:03:24 +0200

    panda

--- packages/cli/lib/panda.js
+++ packages/cli/lib/panda.js
@@ -1,26 +1,12 @@
-module.exports = `                      ..
-,kZLi-\`.
--.  .;~nUL",_;~~;'
-rZZLi\`    .\`\`'\`':~!~".
--ai        .\`'\`\`\`\`'-"~""\;.
-_::[5Luy\.   .'\`\`\`'-"[TNQQ@Pot|!",.
-;;ZQO%&QQOi   \`~\`"I8WQQMQMQQQQMMQW@m$oi\`
-.t%EOQQQQQL   .|PWQQMMMMMMMMMMMMMMMMMMQW8i.
-";:,cTZXOZo,  .:LQQQMMMMMMMMMMMMMMMMMMMQQQ@L;
-\`s_      ....._?8QMMMMMMMMMMMMMMMMMMMMMMQNL!.
-\`jTZ@Qw;   -jT8Z%mDQQMMMMMMMMMMMMMMMMMMMMMQ8v.
-.~o%WQMQQQQQ@%mWQQMMMMMMMMMMMMMMMMMMMMMMMMMQOo:
-_LNQQQQQQQQQQQQQQmFy[s?c}oLgE@QMMMMMMMMMMMQmj.
-.;:tyhNQQQQQQR%x".           .:|}LU%m%PYn}j"
--rn%RRi                    .'\`\`\`\`\`\`,|
-:l;                    .\`\`\`\`\`\`\`\`s
-/"                     .''\`\`\`\`\`-|;
-.|;                       .''\`\`\`\`\`\`"_
-!-                         \`'\`\`\`\`\`\`\`~~
-/                         .\`\`\`\`\`'_!rlI!
-~Lj!-                      .'\`'nZ@WQQQX"
-.[XQQQQKOn"                 .\`'_JXQQQQQQQQQb.
-:uNQQQQQQQQQ@o.            ..\`-\`|%QQQQQQQQQQQ@"
-"EQQQQQQQQQQQQWEj'\`\`\`\`\`..\`'-\`\`'-[@QQQQQMQQ
-\`ZQQQQQQQQQQQQQQQD2|;""!!!!|~~_;nWQQQQQQQQQQQQQq.
-tQQQQQQQQQQ@X%qyr!:::,:::::::_;"|}TOQQQQQQQQQQn`;
+module.exports = `
+      MM.         .MM    |            |
+      "MM._______.MM"    |   GENMA    |
+      /             \    |    CLI!    |
+    /   dMMb   dMMb   \  |            |
+   /  dM"""Mb dM"""Mb  \ |____________|
+  |   MMMMM"/O\"MMMMM   |      ||o
+  |   "MMM"/   \"MMM"   |   .dMMM 8
+  |                     dMMMMMMMM
+  \      \       /     dMMMMMMMP
+AMMMMMMMMM\_____/MMMMMMMMMMMM"
+`;
