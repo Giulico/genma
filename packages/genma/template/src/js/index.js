aaaad818501fb9ce3cd990c3575d88b72fc630f0
giuliocollesei <giuliocollesei@gmail.com>
2020-08-03 22:35:31 +0200

    update template/src

--- packages/genma/template/src/js/index.js
+++ packages/genma/template/src/js/index.js
@@ -1,1 +1,8 @@
+import "../doc/css/style.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import Sidebar from "../doc/sidebar/sidebar.mdx";
+
 console.log("Genma - Start here!");
+
+ReactDOM.render(<Sidebar />, document.getElementById("nav"));
